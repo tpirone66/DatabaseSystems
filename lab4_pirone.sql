155d9485f635c9d5eca1ba0f1ea1238388c94947
tpirone66 <trevor.pirone1@marist.edu>
2017-09-24 11:25:41 -0400

    Trevor Pirone Lab Assignment #4
    8. Check constraints are used to limit values on a single column or can be used on a table that limits the values in certain columns based on values in other columns in the row. Check constraints can be very useful if a value needs to be within a specific range. A good use of a check constraint would be for values such as price and quantity of products. The price of goods must be a positive number as well as the quantity available.
    
    CREATE TABLE Books
    ...,
    CONSTRAINT PRICE >= 0
    ...
    CONSTRAINT QUANTITY >= 0
    
    An example of a bad constraint would be to compare the length of a text value by using a numeric value.
    NAME TEXT CHECK (NAME > 0).

---
+++ lab4_pirone.sql
@@ -0,0 +1,95 @@
+﻿--- Trevor Pirone ---
+--- Databse Management ---
+--- 9/23/17 ---
+--- Lab Assignment #4 ---
+
+------------ Query #1 ----------------------
+
+SELECT city
+FROM Agents
+WHERE aid IN (SELECT aid
+	      FROM Orders
+	      WHERE cid IN(SELECT cid
+			   FROM Customers
+			   WHERE cid = 'c006'
+			  )
+	      );
+	      
+--------------------------------------------
+
+------------ Query #2 ----------------------
+
+SELECT DISTINCT pid
+FROM Orders
+WHERE aid IN (SELECT aid
+	      FROM Orders
+	      WHERE cid = 'c006'
+	     )
+ORDER BY pid DESC;
+
+--------------------------------------------
+
+------------ Query #3 ----------------------
+
+SELECT cid, name 
+FROM Customers
+WHERE cid NOT IN (SELECT cid 
+		  FROM Orders 
+		  WHERE aid = 'a03'
+		 );
+	     
+--------------------------------------------
+
+------------ Query #4 ----------------------
+
+SELECT DISTINCT cid
+FROM Customers
+WHERE cid IN (SELECT cid
+	      FROM Orders
+	      WHERE pid = 'p01' OR pid = 'p07'
+	     )
+ORDER BY cid ASC;
+	     
+--------------------------------------------
+
+------------ Query #5 ----------------------
+
+SELECT DISTINCT pid
+FROM Orders
+WHERE pid NOT IN (SELECT cid
+                  FROM Orders
+                  WHERE aid IN ('a02', 'a03')
+                 )
+ORDER BY pid DESC; 
+	  
+--------------------------------------------
+
+------------ Query #6 ----------------------
+
+SELECT name, discountPct, city 
+FROM Customers
+WHERE cid IN (SELECT cid
+	      FROM Orders
+	      WHERE aid IN (SELECT aid
+			    FROM Agents
+			    WHERE city = 'Tokyo' OR city = 'New York'
+			   )
+	     );
+	     
+--------------------------------------------
+
+------------ Query #7 ----------------------
+
+SELECT *
+FROM Customers
+WHERE discountPct IN (SELECT discountPct
+		      FROM Customers
+	              WHERE city = 'Duluth' OR city = 'London'		     
+	              )
+/*The part below is needed because the query with output the customers from Duluth and London.
+This is okay, however, the query is incomplete because if another customer from a different city 
+is added to the database and has the same discount price as someone from Duluth or London,
+it will not output properly. */	              
+AND city != 'Duluth' AND city != 'London';
+	     
+--------------------------------------------

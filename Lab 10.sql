91a48ea0dd71643d47770138e13e0acdff007132
tpirone66 <trevor.pirone1@marist.edu>
2017-11-24 15:12:44 -0500

    Lab 10 Trevor Pirone
    Lab 10 Trevor Pirone

---
+++ Lab 10.sql
@@ -0,0 +1,37 @@
+﻿---Trevor Pirone---
+---DBMS 308---
+---Lab #10---
+---11/24/17---
+---Prerequesite Function---
+
+---Returns the immediate prerequisites for the passed-in course	number.---
+CREATE OR REPLACE FUNCTION Prerequisite(INT, REFCURSOR) RETURNS refcursor AS 
+$$
+DECLARE
+   input_course INT := $1;
+   resultset REFCURSOR := $2;
+BEGIN
+   open resultset FOR 
+      SELECT preReqNum
+      FROM Prerequisites
+      WHERE courseNum = input_course;
+   RETURN resultset;
+END;
+$$ 
+LANGUAGE plpgsql;
+
+---Returns the courses for which the passed-in course number is an immediate pre-requisite.---
+CREATE OR REPLACE FUNCTION isPrerequisite(INT, REFCURSOR) RETURNS refcursor AS
+$$
+DECLARE
+   input_course INT := $1;
+   resultset REFCURSOR := $2;
+BEGIN
+   open resultset FOR 
+      SELECT courseNum
+      FROM Prerequisites
+      WHERE preReqNum = input_course;
+   RETURN resultset;
+END;
+$$ 
+LANGUAGE plpgsql;
